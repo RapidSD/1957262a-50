2ddbb6c82aed1f34e496dfce59a5318deaf64663
Sergey <s@umbrellab.com>
2025-08-06 11:08:14 +1000

    Deployed from Bolt.new - 2025-08-06T01:08:13.901Z

---
+++ open-next.config.ts
@@ -0,0 +1,7 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+  // Basic configuration for Shopify Next.js app on Cloudflare Workers
+  // This enables the app to run on Cloudflare's edge network
+  // with optimal performance for e-commerce workloads
+});
